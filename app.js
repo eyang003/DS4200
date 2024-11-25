22fe91020beed78060f323fac2512567ed58c841
Tiomot <152098019+FenghaozheTiomot@users.noreply.github.com>
2024-11-25 15:00:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
 
-menu.addEventListener('click',function(){
+menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
-<div class="button-container">
-    <button onclick="window.location.href='https://github.com/eyang003/DS4200';">
-        Click the Link to star our project in github!
-    </button>
-</div>
